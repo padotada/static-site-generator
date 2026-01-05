e7b2f3002cf8165c72eb365da5c4c1bd870f0e0c
padotada <roypark2005@gmail.com>
2026-01-05 11:21:56 -0800

    Wrote test cases using unittest to make sure the TextNode works correctly

---
+++ src/test_textnode.py
@@ -0,0 +1,47 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq_plain(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        node2 = TextNode("This is a text node", TextType.PLAIN)
+        self.assertEqual(node, node2)
+        
+    def test_eq_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+    
+    def test_eq_italic(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertEqual(node, node2)
+        
+    def test_eq_code(self):
+        node = TextNode("This is a text node", TextType.CODE)
+        node2 = TextNode("This is a text node", TextType.CODE)
+        self.assertEqual(node, node2)
+    
+    def test_eq_link(self):
+        node = TextNode("This is a text node", TextType.LINK)
+        node2 = TextNode("This is a text node", TextType.LINK)
+        self.assertEqual(node, node2)
+        
+    def test_eq_with_url(self):
+        node = TextNode("This is a text node", TextType.LINK, "boot.dev")
+        node2 = TextNode("This is a text node", TextType.LINK, "boot.dev")
+        self.assertEqual(node, node2)
+
+            
+    def test_neq_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is the second text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+    
+   
+
+
+if __name__ == "__main__":
+    unittest.main()
