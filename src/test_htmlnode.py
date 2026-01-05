60af5734d995ffde06862b8fcb44ed066240ad33
padotada <roypark2005@gmail.com>
2026-01-05 14:33:34 -0800

    Created test cases for LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,6 @@
 import unittest
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_obj_repr(self):
@@ -21,5 +22,17 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("body", None, ["h2"], {"class": "some_class"})
         self.assertEqual(node.props_to_html(), ' class="some_class"')
 
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_a_with_prop(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), """<a href="https://www.google.com">Click me!</a>""")
+    
+    def test_leaf_to_html_link(self):
+        node = LeafNode("label", "Last name:", {"for":"lname"})
+        self.assertEqual(node.to_html(), """<label for="lname">Last name:</label>""")
 if __name__ == "__main__":
     unittest.main()
